196d9cb3f04e9140ae6ab12a288b6fd4bac9ac20
SYSKUKU <69404608+syskuku@users.noreply.github.com>
2024-07-29 02:21:14 +0800

    Update site.config.js
    更改目录

--- config/site.config.js
+++ config/site.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
   // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
   // your email being exposed in public.
-  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'syskuku@imikufans.onmicrosoft.com',
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'syskuku@imikufans.com',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '切换主站： <a href="https://blog.imikufans.fun" target="_blank" rel="noopener noreferrer">SYSKUKU研究部</a>. 感谢：Made with ❤ by SpencerWoo.',
+    '切换主站： <a href="https://blog.imikufans.com" target="_blank" rel="noopener noreferrer">SYSKUKU研究部</a>. 感谢：Made with ❤ by SpencerWoo.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
